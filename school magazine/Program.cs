b858f3b2b0fc32a8f1cd568fe36fcd5d3bd6e994
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2023-11-17 16:41:38 +0300

    Update Program.cs

--- school magazine/Program.cs
+++ school magazine/Program.cs
@@ -3,12 +3,10 @@ using school_magazine;
 Console.WriteLine("\t******************************************");
 Console.WriteLine("\t\tМИНСКАЯ СРЕДНЯЯ ШКОЛА\n");
 Console.Write("\t******************************************\n");
-string line;
-
 while (true)
 {
     var magazin = new Magazin();
-    Console.WriteLine("\n\tВыберите цифру:\n\t1)Вывести список учащихся \n\t2)Добавить запись \n\t3)Удалить запись \n\t4)Редактировать запись \n\t5)Выход");
+    Console.WriteLine("\n\tВыберите цифру:\n\t1)Вывести список учащихся \n\t2)Добавить запись \n\t3)Удалить запись \n\t4)Редактировать запись\n\t5)Вывести учеников с одной параллели \n\t6)Выход");
     Console.Write("\n\t");
     int number = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine();
@@ -16,5 +14,7 @@ while (true)
     if (number == 2) magazin.NewEntry();
     if (number == 3) magazin.DeleteEntry();
     if (number == 4) magazin.EditEntry();
-    if (number == 5) break;
+    if (number == 5) magazin.PrintPeople();
+    if (number == 6) break;
+
 }
