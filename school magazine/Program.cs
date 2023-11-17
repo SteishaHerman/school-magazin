35b4c731097d0c6e796fb06b0ce3040670e3e43c
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2023-11-17 16:18:43 +0300

    Update Program.cs

--- school magazine/Program.cs
+++ school magazine/Program.cs
@@ -1,48 +1,20 @@
-﻿Console.WriteLine("\t******************************************");
+using school_magazine;
+
+Console.WriteLine("\t******************************************");
 Console.WriteLine("\t\tМИНСКАЯ СРЕДНЯЯ ШКОЛА\n");
-Console.WriteLine("\t******************************************\n\n");
+Console.Write("\t******************************************\n");
 string line;
-//string[] informationFile;
-FileInfo fileInfo = new FileInfo(@"C:\Users\Agerman\source\repos\school magazine\student.txt");
-if (!fileInfo.Exists)
+
+while (true)
 {
-    StreamWriter writeFile = File.CreateText(@"C:\Users\Agerman\source\repos\school magazine\student.txt");
-    Console.Write("Был создан файл, пожалуйста заполните следующие данные:\n");
-    NewEntry();
+    var magazin = new Magazin();
+    Console.WriteLine("\n\tВыберите цифру:\n\t1)Вывести список учащихся \n\t2)Добавить запись \n\t3)Удалить запись \n\t4)Редактировать запись \n\t5)Выход");
+    Console.Write("\n\t");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    if (number == 1) magazin.ReadEntry();
+    if (number == 2) magazin.NewEntry();
+    if (number == 3) magazin.DeleteEntry();
+    if (number == 4) magazin.EditEntry();
+    if (number == 5) break;
 }
-ReadEntry();
-Console.WriteLine("\n");
-Console.WriteLine("\tВыберите цифру:\n\t1)Вывести список учащихся \n\t2)Добавить запись \n\t3)Удалить запись \n\t4)Выход ");
-int number = Convert.ToInt32(Console.ReadLine());
-void DeleteEntry()
-{   }
-void ReadEntry()
-{
-    StreamReader readFile = new StreamReader(@"C:\Users\Agerman\source\repos\school magazine\student.txt");
-    line = readFile.ReadLine();
-    while (line != null)
-    {
-        Console.WriteLine("\t" + line);
-        line = readFile.ReadLine();
-    }
-    readFile.Close();
-}
-void NewEntry()
-{
-    StreamWriter writeFile = File.AppendText(@"C:\Users\Agerman\source\repos\school magazine\student.txt");
-    string readInf;
-    Console.Write("1)Id: ");
-    readInf = Console.ReadLine();
-    writeFile.Write(readInf + " ");
-    Console.Write("\n2)ФИО школьника: ");
-    readInf = Console.ReadLine();
-    writeFile.Write(readInf+" ");
-    Console.Write("\n3)Номер класса: ");
-    readInf = Console.ReadLine();
-    writeFile.Write(readInf+" ");
-    Console.Write("\n4)Буква класса: ");
-    readInf = Console.ReadLine();
-    writeFile.Write(readInf+"\n");
-    writeFile.Close();
-}//
-Console.WriteLine();
