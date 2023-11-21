d004f1c0f86a96a6b416a8ea94727610a53c1d25
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2023-11-21 17:56:23 +0300

    Update StudentRepository.cs

--- school magazine/StudentRepository.cs
+++ school magazine/StudentRepository.cs
@@ -5,11 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace schoolMagazine
 {
     internal class StudentRepository : IStudentRepository
     {
-        public StudentRepository() { }
+        public IInformationStudent _studentInfo;
+        public StudentRepository(IInformationStudent studentInfo) 
+        {
+            _studentInfo = studentInfo;
+        }
         public void CreateNewEntry(Student student)
         {
             using (StreamWriter writeFile = File.AppendText(@"C:\Users\Agerman\source\repos\school magazine\student.txt"))
@@ -22,14 +27,13 @@ namespace schoolMagazine
                 writeFile.WriteLine(student.LetterClass);
             }
         }
-
-        public void DeleteEntry(Student student,InformationStudent studentInfo)
+        public void DeleteEntry(Student student)
         {
             int index = -1;
             var lines = File.ReadAllLines(@"C:\Users\Agerman\source\repos\school magazine\student.txt").ToList();
             string value = student.Id+" "+student.Surname+" "+student.Name+" "+student.TwoName+" "+student.NumberClass+" "+student.LetterClass;
             index = lines.IndexOf(value);
-            if (index == -1) studentInfo.NotFoundStudent();
+            if (index == -1) _studentInfo.NotFoundStudent();
             else
             {
                 lines.RemoveAt(index);
@@ -37,24 +41,24 @@ namespace schoolMagazine
             }
         }
 
-        public void EditEntry(Student student, InformationStudent studentInfo)
+        public void EditEntry(Student student)
         {
             int index = -1;
             var allPeople = File.ReadAllLines(@"C:\Users\Agerman\source\repos\school magazine\student.txt").ToList();
             string oldInformation = student.Id + " " + student.Surname + " " + student.Name + " " + student.TwoName + " " + student.NumberClass + " " + student.LetterClass;
             index = allPeople.IndexOf(oldInformation);
-            if (index == -1) studentInfo.NotFoundStudent();
+            if (index == -1) _studentInfo.NotFoundStudent();
             else
             {
-                studentInfo.EditEntry();
-                student = studentInfo.InformationAboutStudent();
+                _studentInfo.EditEntry();
+                student = _studentInfo.InformationAboutStudent();
                 string newInformation = student.Id + " " + student.Surname + " " + student.Name + " " + student.TwoName + " " + student.NumberClass + " " + student.LetterClass;
                 allPeople[index] = newInformation;
                 File.WriteAllLines(@"C:\Users\Agerman\source\repos\school magazine\student.txt", allPeople);
             }
         }
 
-        public void PrintAll(InformationStudent studentInfo)
+        public void PrintAll()
         {
             string line;
             using (StreamReader readFile = new StreamReader(@"C:\Users\Agerman\source\repos\school magazine\student.txt"))
@@ -62,20 +66,20 @@ namespace schoolMagazine
                 line = readFile.ReadLine();
                 while (line != null)
                 {
-                    studentInfo.PrintAll(line);
+                    _studentInfo.PrintAll(line);
                     line = readFile.ReadLine();
                 }
             }
         }
-        public void PrintAllIdenticalPeople(InformationStudent studentInfo)
+        public void PrintAllIdenticalPeople()
         {
-            int number = studentInfo.EnterIdenticalPeople();
+            int number = _studentInfo.EnterIdenticalPeople();
             var allPeople = File.ReadAllLines(@"C:\Users\Agerman\source\repos\school magazine\student.txt").ToList();
             string[] oneChild;
             foreach (var item in allPeople)
             {
                 oneChild = item.Split(" ");
-                if (oneChild[4] == Convert.ToString(number)) studentInfo.PrintIdenticalPeople(item);
+                if (oneChild[4] == Convert.ToString(number)) _studentInfo.PrintIdenticalPeople(item);
             }
         }
     }
