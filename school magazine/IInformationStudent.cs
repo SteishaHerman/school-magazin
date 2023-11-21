bbeebac76ea0365e6fc30045167168cf5f90645a
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2023-11-21 17:57:06 +0300

    Add files via upload

---
+++ school magazine/IInformationStudent.cs
@@ -0,0 +1,20 @@
+﻿using schoolMagazine.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolMagazine
+{
+    internal interface IInformationStudent
+    {
+        public Student InformationAboutStudent();
+        public void NotFoundStudent();
+        public void EditEntry();
+        public void PrintAll(string line);
+        public int EnterIdenticalPeople();
+        public void PrintIdenticalPeople(string item);
+
+    }
+}
