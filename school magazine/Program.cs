caf43979d7e5794813a425e255e264f8e02aa6f3
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2023-11-21 12:20:50 +0300

    Update Program.cs

--- school magazine/Program.cs
+++ school magazine/Program.cs
@@ -1,20 +1,32 @@
 using school_magazine;
+using school_magazine.models;
 
 Console.WriteLine("\t******************************************");
 Console.WriteLine("\t\tМИНСКАЯ СРЕДНЯЯ ШКОЛА\n");
 Console.Write("\t******************************************\n");
 while (true)
 {
-    var magazin = new Magazin();
     Console.WriteLine("\n\tВыберите цифру:\n\t1)Вывести список учащихся \n\t2)Добавить запись \n\t3)Удалить запись \n\t4)Редактировать запись\n\t5)Вывести учеников с одной параллели \n\t6)Выход");
     Console.Write("\n\t");
     int number = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine();
-    if (number == 1) magazin.ReadEntry();
-    if (number == 2) magazin.NewEntry();
-    if (number == 3) magazin.DeleteEntry();
-    if (number == 4) magazin.EditEntry();
-    if (number == 5) magazin.PrintPeople();
+    StudentRepository studentRepository = new StudentRepository();
     if (number == 6) break;
-
+    if (number == 1)
+    {
+        studentRepository.PrintAll();
+        continue;
+    }
+    if (number == 5)
+    {
+        studentRepository.PrintAllIdenticalPeople();
+        continue;
+    }
+    Student student;
+    InformationStudent studentInfo = new InformationStudent();
+    student = studentInfo.InformationAboutStudent();
+    if (number == 2) studentRepository.CreateNewEntry(student);
+    if (number == 3) studentRepository.DeleteEntry(student);
+    if (number == 4) studentRepository.EditEntry(student, studentInfo);
 }
+
