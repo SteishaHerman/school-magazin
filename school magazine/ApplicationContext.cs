fe64e11269f04039cc8f0afa1e0258e70adaaf06
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2024-01-03 15:17:13 +0300

    Add files via upload

---
+++ school magazine/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolMagazine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace School_Magazine
+{
+    internal class ApplicationContext : DbContext
+    {
+        string section { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public ApplicationContext(string inf)
+        {
+            this.section = inf;
+
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(section);
+        }
+    }
+} 
