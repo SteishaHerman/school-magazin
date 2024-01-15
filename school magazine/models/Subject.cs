bc8018fd87bcd21e6b7adbda38301d240d058e1d
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2024-01-15 11:26:03 +0300

    new models

---
+++ school magazine/models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMagazine.Models
+{
+    public class Subject
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity),Required, Key]  
+        public int Id { get; set; }
+        [Column ("Name_Subject")]
+        public string NameSubject { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ school magazine/models/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMagazine.Models
+{
+    public class Teacher
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity),Required,Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname {  get; set; }
+        public List<Subject> Subjects { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
