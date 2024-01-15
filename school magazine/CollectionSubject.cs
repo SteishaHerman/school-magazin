eb824de48840031903b38651ba4031490f8fab20
SteishaHerman <78555402+SteishaHerman@users.noreply.github.com>
2024-01-15 11:26:55 +0300

    Add files via upload

---
+++ school magazine/CollectionSubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Magazine
+{
+    public class CollectionSubject
+    {
+        public string  SubjectName { get; set; }
+        public int CountSubject {  get; set; }
+    }
+}
